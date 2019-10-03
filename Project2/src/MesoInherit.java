a8b1ae23551304f97f1782b60826a8218413bcb5
Robyn Pollock <pollock.robyn.j@gmail.com>
2019-10-03 11:47:10 -0500

    added methods abstract methods to MesoInherit

--- Project2/src/MesoInherit.java
+++ Project2/src/MesoInherit.java
@@ -2,6 +2,18 @@
  * @author Robyn Pollock
  * @version 2019-10-3
 */
-public class MesoInherit {
-	//TODO: Generate inteded output from line 3 to line 6. inherit the abstract class/method as necessary
+public class MesoInherit extends MesoAbstract {
+
+	@Override
+	public int[] calAverage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public char letterAverage() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	//TODO: Generate intended output from line 3 to line 6. inherit the abstract class/method as necessary
 }
